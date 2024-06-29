28a2ccbb2b0d60734da4db8fac003443f9d5ac02
vierodan <vierodan@gmail.com>
2024-06-29 21:58:20 +0200

    main.tf amended

--- cp2/terraform/main.tf
+++ cp2/terraform/main.tf
@@ -5,8 +5,18 @@ terraform{
       version = "~> 3.0.2"
     }
   }
-  required_version = "1.1.0"
+  required_version = ">= 1.1.0, < 2.0.0"
 }
 provider "azurerm" {
   features {}
+}
+
+// Module resources to call the network module
+module "resources" {
+  source = "./modules/resources"
+
+  // Optional: You can pass variables to the module if needed
+  // Example:
+  // vnet_name   = "exampleVNet"
+  // subnet_name = "subnet1"
 }

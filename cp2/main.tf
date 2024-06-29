de852dd655f2eb7878e64b0487393f325b7549ce
vierodan <vierodan@gmail.com>
2024-06-29 13:53:15 +0200

    main file created

---
+++ cp2/main.tf
@@ -0,0 +1,40 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "avrcp2azrg" {
+  name     = "avrcp2azrg"
+  location = "West Europe"
+
+  tags = {
+    environment = "development"
+    project     = "avr-unir-devops-cp2"
+  }
+}
+
+resource "azurerm_container_registry" "avrcp2azacr" {
+  name                     = "avrcp2azacr"
+  resource_group_name      = azurerm_resource_group.avrcp2azrg.name
+  location                 = azurerm_resource_group.avrcp2azrg.location
+  sku                      = "Basic"  # Available SKUs: Basic, Standard, Premium
+
+  admin_enabled            = true  # Set to false if you do not want admin user enabled
+
+  tags = {
+    environment = "development"
+    project     = "avr-unir-devops-cp2"
+  }
+}
+
+output "acr_login_server" {
+  value = azurerm_container_registry.avrcp2azacr.login_server
+}
+
+output "acr_admin_username" {
+  value = azurerm_container_registry.avrcp2azacr.admin_username
+}
+
+output "acr_admin_password" {
+  value     = azurerm_container_registry.avrcp2azacr.admin_password
+  sensitive = true
+}

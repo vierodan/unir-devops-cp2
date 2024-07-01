721efd880b3754ba991e806b0b56451a50a6c18f
vierodan <vierodan@gmail.com>
2024-07-01 14:17:24 +0200

    acr.tf added

---
+++ cp2/terraform/acr.tf
@@ -0,0 +1,15 @@
+# acr.tf
+
+resource "azurerm_container_registry" "acr" {
+  name                = var.azurerm_container_registry_name_value
+  resource_group_name = var.azurerm_resource_group_name_value
+  location            = var.azurerm_location_name_value
+  sku                 = var.azurerm_sku_basic_name_value # Available SKUs: Basic, Standard, Premium
+
+  admin_enabled = true # Set to false if you do not want admin user enabled
+
+  tags = {
+    environment = "development"
+    project     = "cp2"
+  }
+}
--- cp2/terraform/main.tf
+++ cp2/terraform/main.tf
@@ -11,20 +11,6 @@ resource "azurerm_resource_group" "rg" {
   }
 }
 
-resource "azurerm_container_registry" "acr" {
-  name                = var.azurerm_container_registry_name_value
-  resource_group_name = var.azurerm_resource_group_name_value
-  location            = var.azurerm_location_name_value
-  sku                 = var.azurerm_sku_basic_name_value # Available SKUs: Basic, Standard, Premium
-
-  admin_enabled = true # Set to false if you do not want admin user enabled
-
-  tags = {
-    environment = "development"
-    project     = "cp2"
-  }
-}
-
 
 
 

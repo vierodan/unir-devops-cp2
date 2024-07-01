b77639920102332c6694b5391ca5beb6912fc811
vierodan <vierodan@gmail.com>
2024-07-01 10:17:49 +0200

    main amended

--- cp2/terraform/main.tf
+++ cp2/terraform/main.tf
@@ -1,19 +1,13 @@
 # main.tf
-
 terraform {
-    required_version = ">= 1.9.0"
-
-    required_providers {
-        azurerm = {
-            source  = "hashicorp/azurerm"
-            version = "~> 3.0"
-        }
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "> 3.0"
     }
+  }
 }
 
-provider "azurerm" {
-    features {}
-}
 
 
 

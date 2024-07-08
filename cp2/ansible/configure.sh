ac418f4492d23deb9a0eaeb89522f52e795c96ed
vierodan <vierodan@gmail.com>
2024-07-08 15:50:28 +0200

    some files refactorized

--- cp2/ansible/configure.sh
+++ cp2/ansible/configure.sh
@@ -1,5 +1,2 @@
-chmod +x export_sensitive_data.sh
 chmod +x launch_ansible_playbooks.sh
-
-./export_sensitive_data.sh
 ./launch_ansible_playbooks.sh
--- cp2/ansible/launch_ansible_playbooks.sh
+++ cp2/ansible/launch_ansible_playbooks.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Source of script with sensitive data about azure credentials. (This .sh is not included on the git repository)
+source ./export_sensitive_data.sh
+
 echo "ACR_NAME=$ACR_NAME"
 echo "RESOURCE_GROUP=$RESOURCE_GROUP"
 
@@ -7,7 +10,7 @@ echo "RESOURCE_GROUP=$RESOURCE_GROUP"
 ansible-playbook -i ansible_playbook_host.yml ansible_playbook_copy_images_to_acr.yml
 
 # Run the Ansible playbook
-ansible-playbook -i ansible_vm_ip_inventory.ini ansible_playbook_add_ssh_public_key_to_vm.yml
+ansible-playbook -i ansible_playbook_host.yml ansible_playbook_add_ssh_public_key_to_vm.yml
 
 # Run the Ansible playbook
 ansible-playbook -i ansible_vm_ip_inventory.ini ansible_playbook_install_software_on_vm.yml

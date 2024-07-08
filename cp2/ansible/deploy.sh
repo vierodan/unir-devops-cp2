a6581d888a94af48956be2f6d016fd65e5e3d8b3
vierodan <vierodan@gmail.com>
2024-07-08 14:23:13 +0200

    launch ansible playbook sh file added

--- cp2/ansible/deploy.sh
+++ cp2/ansible/deploy.sh
@@ -1,3 +1,5 @@
-
+chmod +x export_sensitive_data.sh
 chmod +x launch_ansible_playbooks.sh
+
+./export_sensitive_data.sh
 ./launch_ansible_playbooks.sh
---
+++ cp2/ansible/launch_ansible_playbooks.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo "ACR_NAME=$ACR_NAME"
+echo "RESOURCE_GROUP=$RESOURCE_GROUP"
+
+# Run the Ansible playbook
+ansible-playbook -i ansible_playbook_host.yml ansible_playbook_copy_images_to_acr.yml
+
+# Run the Ansible playbook
+ansible-playbook -i ansible_vm_ip_inventory.ini ansible_playbook_add_ssh_public_key_to_vm.yml
+
+# Run the Ansible playbook
+ansible-playbook -i ansible_vm_ip_inventory.ini ansible_playbook_install_software_on_vm.yml
+
+# Run the Ansible playbook
+ansible-playbook -i ansible_vm_ip_inventory.ini ansible_playbook_run_nginx_container_on_vm.yml
+
+
+
+
+
+

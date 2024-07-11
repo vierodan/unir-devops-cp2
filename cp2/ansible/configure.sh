e30d0b8809b4bf84ce064487997b9d5f0601f09e
vierodan <vierodan@gmail.com>
2024-07-11 19:16:28 +0200

    configure file amended

--- cp2/ansible/configure.sh
+++ cp2/ansible/configure.sh
@@ -4,11 +4,11 @@
 source ./export_sensitive_data.sh
 
 # Run the Ansible playbook - create images on ACR using Skopeo
-export ANSIBLE_PYTHON_INTERPRETER=/Users/angelviejobueno/myenv/bin/python3.12
+export ANSIBLE_PYTHON_INTERPRETER=~/myenv/bin/python3.12
 ansible-playbook -i ansible_playbook_host.yml ansible_playbook_copy_images_to_acr.yml
 
 # Run the Ansible playbook - Add SSH public key to Azure VM
-export ANSIBLE_PYTHON_INTERPRETER=/Users/angelviejobueno/myenv/bin/python3.12
+export ANSIBLE_PYTHON_INTERPRETER=~/myenv/bin/python3.12
 ansible-playbook -i ansible_playbook_host.yml ansible_playbook_add_ssh_public_key_to_vm.yml
 
 # Run the Ansible playbook - Install all necessary softwre on Azure VM
@@ -20,7 +20,7 @@ export ANSIBLE_PYTHON_INTERPRETER=/usr/bin/python3.10
 ansible-playbook -i ansible_vm_ip_inventory.ini ansible_playbook_run_nginx_container_on_vm.yml
 
 #Run the Ansible playbook - Deploy containers on AKS
-export ANSIBLE_PYTHON_INTERPRETER=/Users/angelviejobueno/myenv/bin/python3.12
+export ANSIBLE_PYTHON_INTERPRETER=~/myenv/bin/python3.12
 ansible-playbook -i ansible_playbook_host.yml ansible_playbook_deploy_containers_on_aks.yml
 
 
